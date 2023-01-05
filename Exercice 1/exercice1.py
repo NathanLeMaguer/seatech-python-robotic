35c380177e03a358b824b6a357c85e796ac87e3f
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-05 16:53:11 +0100

    Base programme fonctionnel

--- Exercice 1/exercice1.py
+++ Exercice 1/exercice1.py
@@ -1,14 +1,21 @@
-class Robot:
-	
-    __slots__ = ('name', 'power','speed')
-	
-    
-	
-    def __init__(self, name='', power=False, speed=0):
-	
-        self.name = name
-        self.power = power
-        self.speed = speed 
+class Robot():
 
+    __slots__ = ('name', 'power','speed') 
+
+    __name = "<unnamed>"
+    __power = False
+    __current_speed = 0
+    __battery_level = 0
+    __states = ['shutown', 'running']
         
+    # Getter / Setter
 
+    def get_battery_level(self):
+        return self.__battery_level
+
+    def set_battery_level(self):
+        self.__battery_level = 2
+
+r = Robot()
+
+print(r.get_battery_level())
