8cd66350fd03bf42b8e9baa204d63d26ff0da698
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-05 17:08:55 +0100

    Suite programme Exo 1

--- Exercice 1/exercice1.py
+++ Exercice 1/exercice1.py
@@ -1,6 +1,6 @@
 class Robot():
 
-    __slots__ = ('name', 'power','speed') 
+   # __slots__ = ('name', 'power','speed') 
 
     __name = "<unnamed>"
     __power = False
@@ -13,9 +13,29 @@ class Robot():
     def get_battery_level(self):
         return self.__battery_level
 
-    def set_battery_level(self):
-        self.__battery_level = 2
+    def set_battery_level(self, battery_level):
+        self.__battery_level = battery_level
+
+    def get_current_speed(self):
+        return self.__current_speed
+
+    def set_current_speed(self, current_speed):
+        self.__current_speed = current_speed
+
+    def get_power(self):
+        return self.__power
+
+    def set_power_on(self):
+        print('Power ON')
+        __power = True 
+
+    def set_power_off(self):
+        print('Power OFF')
+        __power = False
+
 
 r = Robot()
 
 print(r.get_battery_level())
+r.set_current_speed(50)
+print(r.get_current_speed())
