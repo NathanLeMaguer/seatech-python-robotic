03fc42cbf9f765f8d659d5e501677652e7bf1f7e
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-05 16:33:52 +0100

    exercice1_v0

---
+++ Exercice 1/exercice1.py
@@ -0,0 +1,14 @@
+class Robot:
+	
+    __slots__ = ('name', 'power','speed')
+	
+    
+	
+    def __init__(self, name='', power=False, speed=0):
+	
+        self.name = name
+        self.power = power
+        self.speed = speed 
+
+        
+
