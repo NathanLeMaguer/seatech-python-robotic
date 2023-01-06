df44ed282e37bbecf540613245f7469e775e7bf2
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-06 09:09:19 +0100

    Ensemble fonction - Exercice (détails à pourvoir)

--- Exercice 1/exercice1.py
+++ Exercice 1/exercice1.py
@@ -1,3 +1,5 @@
+import time 
+
 class Robot():
 
    # __slots__ = ('name', 'power','speed') 
@@ -7,7 +9,8 @@ class Robot():
     __current_speed = 0
     __battery_level = 0
     __states = ['shutown', 'running']
-        
+    move_flag = False 
+
     # Getter / Setter
 
     def get_battery_level(self):
@@ -33,9 +36,37 @@ class Robot():
         print('Power OFF')
         __power = False
 
+    def recharge(self,battery_level):
+
+        i = Robot.get_battery_level(self)
+        
+        for i in range(battery_level+1):
+        #for Robot.get_battery_level() < battery_level:
+        
+            Robot.set_battery_level(self,i)
+            time.sleep(0.05)         
+            print("Charge en cours : ",i,"%")
+
+    def move(self):
+
+        if(self.move_flag == False):
+            print("Déplacement du robot en cours ...")
+            self.move_flag = True
+        else:
+            print("Déplacement déjà en cours ...")
+
+        if(Robot.get_current_speed == 0):
+            Robot.set_current_speed(self,50) 
+        return self.move_flag 
+    
+    #def stop(self):
 
 r = Robot()
 
 print(r.get_battery_level())
 r.set_current_speed(50)
 print(r.get_current_speed())
+#r.recharge(50)
+#print("Fin de charge : ",r.get_battery_level(),"%")
+
+r.move()
