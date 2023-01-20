4869f135c5dc6fdb1052e820ff957eadb2abd5c7
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-20 11:31:55 +0100

    Implémentation Webot

---
+++ webots/controllers/my_controller/my_controller.py
@@ -0,0 +1,54 @@
+"""my_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor, DistanceSensor
+
+class FugitiveRobot(Robot):
+    def __init__(self):
+         super().__init__()
+         wheels = FugitiveRobotMotors
+
+class FugitiveRobotMotor(Motor): 
+
+    def __init__(self, name=None):
+        super().__init__(name)
+        self.setPosition(float('inf'))
+        self.setVelocity(0)
+
+class FugitiveRobotMotors():
+    def __init__(self):
+        self.__left_wheel_motor = FugitiveRobotMotor('left wheel motor')
+        self.__right_wheel_motor = FugitiveRobotMotor('right wheel motor')
+
+    def go_front(self):
+        self.__left_wheel_motor.setVelocity(5)
+        self.__right_wheel_motor.setVelocity(5)
+
+
+# create the Robot instance.
+robot = FugitiveRobot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getDevice('motorname')
+#  ds = robot.getDevice('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    robot.go_front()
+
+# Enter here exit cleanup code.
