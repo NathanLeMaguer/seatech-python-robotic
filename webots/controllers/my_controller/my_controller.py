e18dc098f4ee400e4a0c83562caa03865bbad0e5
NathanLeMaguer <nathan.lemaguer@gmail.com>
2023-01-23 10:09:48 +0100

    Correction None Value by Enable Sensor Distance

--- webots/controllers/my_controller/my_controller.py
+++ webots/controllers/my_controller/my_controller.py
@@ -13,8 +13,8 @@ from controller import Robot, Motor, DistanceSensor
 class FugitiveRobot(Robot):
     def __init__(self):
          super().__init__()
-         self.__motors = FugitiveRobotMotors()
-         self.__distances = FugitiveRobotSensors()
+         self.motors = FugitiveRobotMotors()
+         self.distances = FugitiveRobotSensors()
 
     def run(self, backward=False, forward=False, left=False, right=False):
         if (forward == True) : 
@@ -26,11 +26,7 @@ class FugitiveRobot(Robot):
         if(right == True) : 
             self.__motors.go_right()
         else : 
-            self.__motors.go_front_fast()
-
-    def distance(self):
-
-        self.__distances.getDistanceValue()
+            self.__motors.go_front_fast()  
 
 class FugitiveRobotMotor(Motor): 
 
@@ -59,28 +55,29 @@ class FugitiveRobotMotors():
     def go_right(self):
         self.__left_wheel_motor.setVelocity(5)
         self.__right_wheel_motor.setVelocity(5)
-
-class FugitiveRobotSensor(DistanceSensor):
-    def __init__(self, name=None):
-        super().__init__(name)
     
 class FugitiveRobotSensors():
     def __init__(self):
-        self.__ds0_sensor = FugitiveRobotSensor('ds0')
-        self.__ds1_sensor = FugitiveRobotSensor('ds1')
-        
+        self.__ds0_sensor = DistanceSensor('ds0')
+        self.__ds1_sensor = DistanceSensor('ds1')
+    
+    def enable(self,timestep:int) -> None :
+        self.__ds0_sensor.enable(timestep)
+        self.__ds1_sensor.enable(timestep)
+
     def getDistanceValue(self):
-        #return [self.__ds0_sensor.getValue(),self.__ds1_sensor]
+        #return [self.__ds0_sensor.getValue(),self.__ds1_sensor.getValue()]
         return self.__ds1_sensor.getValue()
 
-robot = FugitiveRobot()
 
+robot = FugitiveRobot()
 timestep = int(robot.getBasicTimeStep())
 
 while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
-    val = robot.distance()
+    robot.distances.enable(timestep)
+    val = robot.distances.getDistanceValue()
     print(val)
     #robot.run()
 
